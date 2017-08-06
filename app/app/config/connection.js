c227c87150d17e094b8069f2d15d806cff44097c
Kevin Brannan <brannake@gmail.com>
2017-08-05 21:02:04 -0700

    Replaces

--- app/app/config/connection.js
+++ app/app/config/connection.js
@@ -4,23 +4,10 @@
 
 // Dependencies
 var Sequelize = require("sequelize");
-var mysql = require('mysql');
-var connection;
-
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWS_URL);
-} else {
-  connection = mysql.createConnection({
-    host:'localhost',
-    user: 'root',
-    password: 'meatbal1l',
-    database: 'burgers_db'
-  })
-}
 
 // Creates mySQL connection using Sequelize
 var sequelize = new Sequelize("burgers_db", "root", "meatbal1l", {
-  host: "localhost",
+  host: "a07yd3a6okcidwap.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   dialect: "mysql",
   pool: {
     max: 5,
